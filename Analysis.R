250a41f2315612ba70e8abe15c71d154fd41ec19
rohinjain97 <rohinjain97@gmail.com>
2018-07-09 18:22:39 +0200

    New csv format of files

--- Analysis.R
+++ Analysis.R
@@ -1,4 +1,10 @@
 #This code aims to analyse the data we have
 
+
 #Fin Maths Analysis
-BUS2016H<-read.csv("BUS2016H.xlsx",header = TRUE)
+BUS2016H<-read.csv("BUS2016H.csv",header = TRUE, sep=";" )
+BUS2016H<-na.omit(BUS2016H)
+attach(BUS2016H)
+
+no.protest<-subset(BUS2016H,RegAcadYear<2015 & )
+protest<-subset(BUS2016H,RegAcadYear>=2015)
